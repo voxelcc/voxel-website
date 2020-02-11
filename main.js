1cadb919b00a3b2b20107fe283fee328df91bbca
Ben Cobley <32883278+bencobley@users.noreply.github.com>
2020-02-11 12:48:21 +0000

    added side nav bar and adobe fonts

---
+++ main.js
@@ -0,0 +1,11 @@
+//https://codepen.io/jagadeshanh/pen/pNqLYX
+function openSideNav() {
+    //    document.getElementById("ben").classList.toggle("change");
+    document.getElementById("mySidenav").classList.toggle("openclose");
+}
+
+
+function closeSideNav() {
+    //    document.getElementById("ben").classList.toggle("change");
+    document.getElementById("mySidenav").classList.toggle("openclose");
+}
