efcbc2681f11d7b2ac2f6eaa482fe7800df3f34d
Ben Cobley <32883278+bencobley@users.noreply.github.com>
2020-02-11 18:19:33 +0000

    added various sections to index - youtube, main page, kickstarter etc.

--- main.js
+++ main.js
@@ -9,3 +9,22 @@ function closeSideNav() {
     //    document.getElementById("ben").classList.toggle("change");
     document.getElementById("mySidenav").classList.toggle("openclose");
 }
+
+
+$(document).ready(function () {
+    // Set iframe attributes when the show instance method is called
+    $("#videoModal").on("show.bs.modal", function (event) {
+        let button = $(event.relatedTarget); // Button that triggered the modal
+        let url = button.data("video"); // Extract url from data-video attribute
+
+        $(this).find("iframe").attr({
+            src: url,
+            allow: "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        });
+    });
+
+    // Remove iframe attributes when the modal has finished being hidden from the user
+    $("#videoModal").on("hidden.bs.modal", function () {
+        $("#videoModal iframe").removeAttr("src allow");
+    });
+});
