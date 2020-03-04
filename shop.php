240e946bdaa3c780e196449457c5c566c04b9b36
ot316 <olithompson@rocketmail.com>
2020-03-04 13:06:29 +0000

    add google sheets api php

--- shop.php
+++ shop.php
@@ -37,6 +37,87 @@ function test_input($data)
     return $data;
 }
 ?>
+<?php
+
+if (php_sapi_name() != 'cli') {
+    throw new Exception('This application must be run on the command line.');
+}
+
+/**
+ * Returns an authorized API client.
+ * @return Google_Client the authorized client object
+ */
+function getClient()
+{
+    $client = new Google_Client();
+    $client->setApplicationName('Google Sheets API PHP Quickstart');
+    $client->setScopes(Google_Service_Sheets::SPREADSHEETS_READONLY);
+    $client->setAuthConfig('googleaccountapikey.json');
+    $client->setAccessType('offline');
+    $client->setPrompt('select_account consent');
+
+    // Load previously authorized token from a file, if it exists.
+    // The file token.json stores the user's access and refresh tokens, and is
+    // created automatically when the authorization flow completes for the first
+    // time.
+    $tokenPath = 'token.json';
+    if (file_exists($tokenPath)) {
+        $accessToken = json_decode(file_get_contents($tokenPath), true);
+        $client->setAccessToken($accessToken);
+    }
+
+    // If there is no previous token or it's expired.
+    if ($client->isAccessTokenExpired()) {
+        // Refresh the token if possible, else fetch a new one.
+        if ($client->getRefreshToken()) {
+            $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+        } else {
+            // Request authorization from the user.
+            $authUrl = $client->createAuthUrl();
+            printf("Open the following link in your browser:\n%s\n", $authUrl);
+            print 'Enter verification code: ';
+            $authCode = trim(fgets(STDIN));
+
+            // Exchange authorization code for an access token.
+            $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
+            $client->setAccessToken($accessToken);
+
+            // Check to see if there was an error.
+            if (array_key_exists('error', $accessToken)) {
+                throw new Exception(join(', ', $accessToken));
+            }
+        }
+        // Save the token to a file.
+        if (!file_exists(dirname($tokenPath))) {
+            mkdir(dirname($tokenPath), 0700, true);
+        }
+        file_put_contents($tokenPath, json_encode($client->getAccessToken()));
+    }
+    return $client;
+}
+
+
+// Get the API client and construct the service object.
+$client = getClient();
+$service = new Google_Service_Sheets($client);
+
+// Prints the names and majors of students in a sample spreadsheet:
+// https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+$spreadsheetId = '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms';
+$range = 'Class Data!A2:E';
+$response = $service->spreadsheets_values->get($spreadsheetId, $range);
+$values = $response->getValues();
+
+if (empty($values)) {
+    print "No data found.\n";
+} else {
+    print "Name, Major:\n";
+    foreach ($values as $row) {
+        // Print columns A and E, which correspond to indices 0 and 4.
+        printf("%s, %s\n", $row[0], $row[4]);
+    }
+} ?>
+
 
 <body class="d-flex flex-column h-100">
 
@@ -134,6 +215,10 @@ function test_input($data)
                     <form method="POST" action="<?php echo htmlspecialchars($_SERVER[" PHP_SELF "]); ?>">
                         <!--Insert this line into action ="" when ready for deploy: -->
                         <div class="form-group">
+                            <label for="inputFeedback">For makers, by makers</label>
+                            <textarea class="form-control" id="inputFeedback" rows="5" placeholder="What do you want to see from this project?" name="message"></textarea>
+
+                            <small id="emailHelp" class="form-text text-muted">If you'd like a reply from us, include your details! </small>
                             <?php
                                     $message = '<h2>Message:</h2><p>'  . $message . '</p>';
                                     $mail = new PHPMailer\PHPMailer\PHPMailer(true);
@@ -155,11 +240,6 @@ function test_input($data)
                                     $mail->send();
                                     echo ('<br><h3 style ="font-size: 20px;"> Thanks for your message </h3><br>');
                                 ?>
-                            <label for="inputFeedback">For makers, by makers</label>
-                            <textarea class="form-control" id="inputFeedback" rows="5" placeholder="What do you want to see from this project?" name="message"></textarea>
-
-                            <small id="emailHelp" class="form-text text-muted">If you'd like a reply from us, include your details! </small>
-
                         </div>
                         <button type="submit" class="btn btn-primary pull-right" value="Clear">Submit</button>
                 </div>
