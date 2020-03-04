beffb2ab6229793802fe56174282876173d849c0
ot316 <olithompson@rocketmail.com>
2020-03-04 13:12:53 +0000

    add google sheets api php

--- shop.php
+++ shop.php
@@ -38,7 +38,7 @@ function test_input($data)
 }
 ?>
 <?php
-
+require __DIR__ . '/vendor/autoload.php';
 if (php_sapi_name() != 'cli') {
     throw new Exception('This application must be run on the command line.');
 }
