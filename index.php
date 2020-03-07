fbcb204276f2ffde46cd74f7d61f811e6f564f33
Oli Thompson <olithompson@rocketmail.com>
2020-03-07 18:38:54 +0000

    final remove file extensions

--- index.php
+++ index.php
@@ -65,7 +65,7 @@ function test_input($data)
         <h3><a href="hub">Hub</a></h3>
         <h3><a href="hub">Projects</a></h3>
         <h4><a href="product">Welcome&nbsp;Home&nbsp;Pack</a></h4>
-        <h4><a href="product #upcoming">Upcoming&nbsp;Releases</a></h4>
+        <h4><a href="product#upcoming">Upcoming&nbsp;Releases</a></h4>
         <h4><a href="about">About&nbsp;us</a></h4>
         <h4><a href="about">Get&nbsp;in&nbsp;touch</a></h4>
     </div>
