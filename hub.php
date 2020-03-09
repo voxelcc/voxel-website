33d568dd57bbcffe462340cb8f91bd144861b8e4
Oli Thompson <olithompson@rocketmail.com>
2020-03-09 18:40:46 +0000

    google analytics test

--- hub.php
+++ hub.php
@@ -5,14 +5,12 @@
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-159632500-1"></script>
     <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
 
-    gtag('config', 'UA-159632500-1');
+        gtag('config', 'UA-159632500-1');
     </script>
-    <!-- Google Analytics -->
-    <?php require_once('google_analytics_head.php'); ?>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
