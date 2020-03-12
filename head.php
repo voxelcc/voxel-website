853742f060e768215e59fe14e4b6485194670a23
Oli Thompson <olithompson@rocketmail.com>
2020-03-12 18:44:03 +0000

    add favicon

--- head.php
+++ head.php
@@ -8,6 +8,9 @@
   gtag('config', 'UA-159632500-1');
 </script>
 
+<!-- Favicon -->
+<link rel="shortcut icon" href="https://voxelmedia.s3.eu-west-2.amazonaws.com/favicon.png" />
+
 <!-- Required meta tags -->
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
