19bb139469f161da47673f629688ae627026e01d
Oli Thompson <olithompson@rocketmail.com>
2020-03-12 18:25:22 +0000

    center about us cariousel

--- about.php
+++ about.php
@@ -38,27 +38,24 @@
 
                 </div>
             </div>
-
-
-
-
-            <div class="slickcarousel mx-auto pt-5">
-                <div><img src="https://voxelmedia.s3.eu-west-2.amazonaws.com/AU/AU2.png" class="slickimage">
-                    <!--                <p>Ben - CEO</p>-->
-                </div>
-                <div><img src="https://voxelmedia.s3.eu-west-2.amazonaws.com/AU/AU3.png" class="slickimage">
-                    <!--                <p>Ellie - COO</p>-->
-                </div>
-                <div><img src="https://voxelmedia.s3.eu-west-2.amazonaws.com/AU/AU4.png" class="slickimage">
-                    <!--                <p>Gabi - CCO</p>-->
-                </div>
-                <div><img src="https://voxelmedia.s3.eu-west-2.amazonaws.com/AU/AU5.png" class="slickimage">
-                    <!--                <p>Melisa - CMO</p>-->
-                </div>
-                <div><img src="https://voxelmedia.s3.eu-west-2.amazonaws.com/AU/AU6.png" class="slickimage">
-                    <!--                <p>Oli - CTO</p>-->
-                </div>
+        </div>
+        <div class="slickcarousel mx-auto pt-5" style="margin: auto;">
+            <div><img src="https://voxelmedia.s3.eu-west-2.amazonaws.com/AU/AU2.png" class="slickimage">
+                <!--                <p>Ben - CEO</p>-->
             </div>
+            <div><img src="https://voxelmedia.s3.eu-west-2.amazonaws.com/AU/AU3.png" class="slickimage">
+                <!--                <p>Ellie - COO</p>-->
+            </div>
+            <div><img src="https://voxelmedia.s3.eu-west-2.amazonaws.com/AU/AU4.png" class="slickimage">
+                <!--                <p>Gabi - CCO</p>-->
+            </div>
+            <div><img src="https://voxelmedia.s3.eu-west-2.amazonaws.com/AU/AU5.png" class="slickimage">
+                <!--                <p>Melisa - CMO</p>-->
+            </div>
+            <div><img src="https://voxelmedia.s3.eu-west-2.amazonaws.com/AU/AU6.png" class="slickimage">
+                <!--                <p>Oli - CTO</p>-->
+            </div>
+        </div>
 
         </div>
 
